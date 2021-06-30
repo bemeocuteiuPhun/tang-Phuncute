2b07e69ad98e943d825f46e5b2f56bd42a58838f
bemeocuteiuPhun <86711280+bemeocuteiuPhun@users.noreply.github.com>
2021-06-30 15:07:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích em <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu em <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
     messDesc: 'em yêu chị nhất lunnn',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'chị cũng iu emmm <3',
     messLink: 'https://www.facebook.com/profile.php?id=100049895533545'
 }
